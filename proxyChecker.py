e5262e15b2e202508516558e1a32f8e4f2d1bca9
Moluma <50881825+Moluma@users.noreply.github.com>
2021-08-31 19:41:01 +0200

    Update proxyChecker.py

--- proxyChecker.py
+++ proxyChecker.py
@@ -2,10 +2,16 @@ import urllib.request
 import threading
 import random
 import argparse
-from time import time
+from time import time, sleep
+
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
 
 def random_user_agent(file='user_agents.txt'):
-	user_agent = (random.choice(open(file).readlines())).replace("\n", "")
+	with open(file, "r") as f:
+		lines = f.readlines()
+		user_agent = random.choice(lines).replace("\n", "")
+		f.close()
+		sleep(0.1)
 	return str(user_agent)
 
 proxyType = ''
@@ -37,7 +43,10 @@ def checker(i):
 	urllib.request.install_opener(opener)
 	global site
 	req = urllib.request.Request(proxyType + '://' + site)
-	req.add_header("User-Agent", random_user_agent())
+	global user_agent
+	if args.random_agent:
+		user_agent = random_user_agent()
+	req.add_header("User-Agent", user_agent)
 	try:
 		global chosenTimeout
 		start_time = time()
@@ -47,8 +56,10 @@ def checker(i):
 		out_file.write(i)
 		if args.verbose:
 			print ("%s works!" % proxy)
-			print('time: ' + str(time_taken) + '\n')
-	except:
+			print('time: ' + str(time_taken))
+			print('user_agent: ' + user_agent +'\n')
+	except Exception as e:
+		print(e)
 		pass
 		if args.verbose:
 			print ("%s does not respond.\n" % proxy)
@@ -61,8 +72,10 @@ parser.add_argument("-p", "--proxy", help="check HTTPS or HTTP proxies", default
 parser.add_argument("-l", "--list", help="path to your list.txt", default='output.txt')
 parser.add_argument("-s", "--site", help="check with specific website like google.com", default='https://google.com/')
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-r", "--random_agent", help="use a random user agent per proxy", action="store_true")
 args = parser.parse_args() # Получение всех аргументов
 txtfile = args.list # list.txt
 site = args.site # Сайт указанный пользователем (default="https://google.com/")
 proxyType = args.proxy # Какие прокси парсить (default="http")
+chosenTimeout = args.timeout
 threading.Thread(target=checkproxy, args=(txtfile,)).start() # старт
